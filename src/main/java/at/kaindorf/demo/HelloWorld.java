6c4edb5bac07c9786ac81be6249cce65765cb673
zivpad18 <zivpad18@htl-kaindorf.at>
2022-02-28 11:16:56 +0100

    test - Felix

--- src/main/java/at/kaindorf/demo/HelloWorld.java
+++ src/main/java/at/kaindorf/demo/HelloWorld.java
@@ -2,6 +2,8 @@ package at.kaindorf.demo;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello WOrld");
+        System.out.println("HALLO FELIX");
     }
 }
